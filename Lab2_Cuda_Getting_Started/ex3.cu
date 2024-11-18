e318308d4e7e6b25e3fe90c534408bcac92c3c00
Sara <saraodqvist@outlook.com>
2024-11-18 11:59:26 +0100

    lab2

---
+++ Lab2_Cuda_Getting_Started/ex3.cu
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <math.h>
+__global__ void multKernel(int n, float* a, float* b, float* c)
+{
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
+
+	for (int i = index; i < n; i+= stride) {
+		c[i] = a[i] * b[i];
+	}
+}
+
+int main() {
+	int N = 1<<24;
+	float *h_a, *h_b, *h_c;
+	float *d_a, *d_b, *d_c;
+	
+	// Allocate host memory
+	h_a = new float[N];
+	h_b = new float[N];
+	h_c = new float[N];
+
+	// Allocate device memory
+	cudaMalloc(&d_a, N * sizeof(float));
+	cudaMalloc(&d_b, N * sizeof(float));
+	cudaMalloc(&d_c, N * sizeof(float));
+
+	// Initialize host data
+	for (int i = 0; i < N; i++)
+	{
+		h_a[i] = 2.0f;
+		h_b[i] = 3.0f;
+	}
+
+	// Copy data from host to device
+	cudaMemcpy(d_a, h_a, N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, h_b, N * sizeof(float), cudaMemcpyHostToDevice);
+
+	// Launch the kernel
+	int blockSize = 256;
+	int numBlocks = (N + blockSize - 1) / blockSize;	
+	multKernel<<<numBlocks, blockSize>>>(N, d_a, d_b, d_c);
+	
+	// Copy result back to host
+	cudaMemcpy(h_c, d_c, N * sizeof(float), cudaMemcpyDeviceToHost);
+
+	// Check result for errors (all values should be 6.0f)
+
+	float maxError = 0.0f;
+	for (int i = 0; i < N; i++)
+		maxError = fmax(maxError, fabs(h_c[i] - 6.0f));
+
+	std::cout << "Max error: " << maxError << std::endl;
+	// Clean up
+
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_c);
+	delete[] h_a;
+	delete[] h_b;
+	delete[] h_c;
+	
+	return 0;
+}
+
